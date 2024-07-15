cdcc5fe032f71b2bad01f3233338382b17d7a0a5
rahulmehta9101 <dme@abhaytrading.in>
2024-07-16 00:18:34 +0530

    add register controller and bug fixes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) //this is for file name
+      
       cb(null, file.originalname)
     }
   })
